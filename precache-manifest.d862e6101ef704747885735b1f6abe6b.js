e0730bb0fd8c47dc10a8b372ea2228bb82afc33a
buihien0109 <49893899+buihien0109@users.noreply.github.com>
2019-10-23 10:04:26 +0700

    Updates

---
+++ precache-manifest.d862e6101ef704747885735b1f6abe6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d54eae71928fbee40dc7ca4d9b49e5e",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "0b735748c67231354dd2",
+    "url": "/react-training/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "161335041c0b0bc707ab",
+    "url": "/react-training/static/css/main.d1cefbad.chunk.css"
+  },
+  {
+    "revision": "0b735748c67231354dd2",
+    "url": "/react-training/static/js/2.bf4ad48b.chunk.js"
+  },
+  {
+    "revision": "161335041c0b0bc707ab",
+    "url": "/react-training/static/js/main.c1f005ad.chunk.js"
+  },
+  {
+    "revision": "c4e6f638d3921d430940",
+    "url": "/react-training/static/js/runtime-main.68945365.js"
+  },
+  {
+    "revision": "d1d9f1db11193ab57d65d74a67592996",
+    "url": "/react-training/static/media/background.d1d9f1db.svg"
+  }
+]);

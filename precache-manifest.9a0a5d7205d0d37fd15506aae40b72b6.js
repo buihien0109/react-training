152a86c2612dd0d7eb968dbee4af0c0f2ea261bf
buihien0109 <buihien01091997@gmail.com>
2019-10-24 14:50:16 +0700

    Updates

---
+++ precache-manifest.9a0a5d7205d0d37fd15506aae40b72b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50683c4082874c02a51f327a2f867376",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "5ab4a64fc01a612e1a9d",
+    "url": "/react-training/static/css/main.cf831e44.chunk.css"
+  },
+  {
+    "revision": "e9a913e2512cb09ce3ab",
+    "url": "/react-training/static/js/2.0e7e613e.chunk.js"
+  },
+  {
+    "revision": "5ab4a64fc01a612e1a9d",
+    "url": "/react-training/static/js/main.8e120a5e.chunk.js"
+  },
+  {
+    "revision": "c4e6f638d3921d430940",
+    "url": "/react-training/static/js/runtime-main.68945365.js"
+  },
+  {
+    "revision": "d1d9f1db11193ab57d65d74a67592996",
+    "url": "/react-training/static/media/background.d1d9f1db.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-training/precache-manifest.d862e6101ef704747885735b1f6abe6b.js"
+  "/react-training/precache-manifest.9a0a5d7205d0d37fd15506aae40b72b6.js"
 );
 
 self.addEventListener('message', (event) => {
